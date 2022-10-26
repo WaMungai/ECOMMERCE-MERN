1dfc371de4c93f964584e39357fe0a5bf4ad4ca5
unknown <joyce.mungai@simpleformations.ke>
2022-10-26 15:41:52 +0300

    Install frontend dependancies

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        <a href='/'>amazona
+        </a>  
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
